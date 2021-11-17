7c717fe1938b49ad23af23cc4c3b2dc4fe13f277
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-11-17 22:59:29 +0100

    add yargs

--- index.js
+++ index.js
@@ -46,3 +46,35 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 }
 
 invokeAction(argv);
+
+// Variant when we use yargs
+// const argv = require('yargs').argv;
+
+// const invokeAction = async ({ action, id, name, email, phone }) => {
+//   switch (action) {
+//     case 'list':
+//           const contacts = await listContacts();
+//           console.table(contacts);
+//       break;
+
+//     case 'get':
+//           const contactsById = await getContactById(id);
+//           console.table(contactsById);
+//       break;
+
+//     case 'add':
+//           const newContacts = await addContact(name, email, phone);
+//           console.table(newContacts);
+//       break;
+
+//     case 'remove':
+//           const deleteContactById = await removeContact(id);
+//           console.table(deleteContactById);
+//       break;
+
+//     default:
+//       console.warn('\x1B[31m Unknown action type!');
+//   }
+// }
+
+// invokeAction(argv);
